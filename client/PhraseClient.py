d8826cbcb755352ff75374f2cd8dfb5f78ee09a9
Mitsuhiro ETO <frn.fnts@gmail.com>
2017-10-09 04:59:23 +0900

    Update PhraseClient

--- client/PhraseClient.py
+++ client/PhraseClient.py
@@ -18,6 +18,9 @@ class PhraseClient:
             "separation": 2
         }
         res = requests.get("https://api.a3rt.recruit-tech.co.jp/text_suggest/v2/predict", params=params).json()
+        if res["status"] != 0:
+            print "Error occurred"
+            raise
         return res["suggestion"]
 
     def check_typo(self, text):
@@ -30,10 +33,12 @@ class PhraseClient:
         if status == 0 or status == 1:
             return status, res
         else:
+            print "Error occurred"
             raise 
 
     def suggestion_iter(self, text):
         while True:
+        #for _ in xrange(3):
             for s in self.__get_suggestions(text):
                 yield s
 
@@ -41,6 +46,7 @@ class PhraseClient:
         for suggestion in self.suggestion_iter(original_text):
             text = original_text + suggestion
             text = re.sub("\s+", "", text)
+            #print text.encode("utf-8")
 
             status, res = self.check_typo(text)
             if status == 0:
@@ -51,5 +57,8 @@ class PhraseClient:
 if __name__ == '__main__':
     client = PhraseClient()
     text = sys.argv[1].decode("shift-jis")
+    if client.check_typo(text)[0] == 1:
+        print "もっといい文を指定してください"
+        exit()
     text = client.process(text)
     print text.encode("utf-8")
